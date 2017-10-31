971558d24e397ee28aea2b09054e626a852f85a4
Christian Zufferey <christian@zufferey.com>
2017-10-31 17:20:08 +0100

    wip

--- bench1.sh
+++ bench1.sh
@@ -5,7 +5,7 @@ cd poubelle
 for i in `seq 1 10`;
 do
         echo $i
-	wget -r -l 1 -np -p http://www-test.epfl.ch/ -o /dev/null &
+	wget -r -l 1 -np -p https://www-test.epfl.ch/ --no-check-certificate -o /dev/null &
 	sleep 0.1
 
 
