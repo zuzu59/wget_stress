c2d320a9eef90196af8e9eb1dfd7a1fec88af59d
Christian Zufferey <christian@zufferey.com>
2017-11-01 17:22:03 +0100

    wip

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #petit script à lancer pour que cela tourne !
-#zf171101.1146
+#zf171101.1716
+
+znbtests=3
+zspeed=100
 
 rm -Rf poubelle
 mkdir poubelle
@@ -11,20 +14,23 @@ function finish {
 }
 trap finish INT
 
-while [ 1 ]; do
-	/usr/bin/time --format='la commande a duré %e secondes' ./bench1.sh 50
-	ps ax |grep wget |wc
+
+for i in `seq 1 $znbtests`;
+do
+        /usr/bin/time --format='la commande a duré %e secondes' ./bench1.sh $zspeed
+        ps ax |grep wget |wc
 done
-
-
-
-
-
-
-
-read -p "appuyer une touche pour continuer"
-
+trap - INT
 watch -n 1 "ps ax |grep wget |wc"
+exit
+
+
+
+
+
+
+#read -p "appuyer une touche pour continuer"
+#watch -n 1 "ps ax |grep wget |wc"
 
 
 
