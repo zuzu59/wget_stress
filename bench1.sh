b503947cb4e8b46e3e122b583e62874e0ed501b4
Christian Zufferey <christian@zufferey.com>
2017-11-01 14:45:44 +0100

    - bien amélioré mes tests, mais ce n'est pas encore parfait

--- bench1.sh
+++ bench1.sh
@@ -2,13 +2,23 @@
 #petit script tout simple pour stresser un serveur web avec wget
 #zf171101.1147
 
+# Teste si les arguments sont bien passés
+if (( $# < 1 ))
+then
+	echo -e "\nusage: ./bench1.sh nombre_de_tests_par_seconde\n"
+	exit
+fi
+
+zspeed=$1
+zsleep=`jq -n 1/$zspeed`
+
 cd poubelle
 
-for i in `seq 1 100`;
+for i in `seq 1 $zspeed`;
 do
         echo $i
-	wget -np -p https://www-test.epfl.ch/ --no-check-certificate -o /dev/null &
-	sleep 0.01
+	wget -np -p https://www-test.epfl.ch/ --no-check-certificate -o /dev/null&
+	sleep $zsleep
 done
 
 
--- start.sh
+++ start.sh
@@ -2,7 +2,23 @@
 #petit script à lancer pour que cela tourne !
 #zf171101.1146
 
-/usr/bin/time --format='la commande a duré %e secondes' ./bench1.sh
+trap exit INT
+
+while [ 1 ]; do
+	/usr/bin/time --format='la commande a duré %e secondes' ./bench1.sh 5
+	ps ax |grep wget |wc
+done
+
+
+
+
+
+
+
+
+read -p "appuyer une touche pour continuer"
+
+watch -n 1 "ps ax |grep wget |wc"
 
 
 
