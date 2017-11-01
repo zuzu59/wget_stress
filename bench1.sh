54c13a456da7846fe09948cf3959fac7d05873f6
Christian Zufferey <christian@zufferey.com>
2017-11-01 11:48:31 +0100

    - refactorisé et amélioré le stress en ne testant que la home page cette fois

--- bench1.sh
+++ bench1.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+#petit script tout simple pour stresser un serveur web avec wget
+#zf171101.1147
 
 cd poubelle
 
-for i in `seq 1 10`;
+for i in `seq 1 100`;
 do
         echo $i
-	wget -r -l 1 -np -p https://www-test.epfl.ch/ --no-check-certificate -o /dev/null &
-	sleep 0.1
-
-
+	wget -np -p https://www-test.epfl.ch/ --no-check-certificate -o /dev/null &
+	sleep 0.01
 done
 
 
--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#petit script à lancer pour que cela tourne !
+#zf171101.1146
 
 /usr/bin/time --format='la commande a duré %e secondes' ./bench1.sh
 
