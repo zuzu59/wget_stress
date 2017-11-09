e9c42e87e58757abbe99061e07f986d678af2a8e
Christian Zufferey <christian@zufferey.com>
2017-11-09 14:03:01 +0100

    - refactorisé le script xmonitor.sh

--- xmonitor.sh
+++ xmonitor.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #petit script tout simple pour monitorer la durée de chargement d'une page web. En fait cela mesure l'expérience utilisateur ;-)
 
-#zf171109.1212
+#zf171109.1400
 
-export TIMEFORMAT="%3R"
-
-znbtests=300
+znbtests=600
 zspeed=0.5
+zurl="https://www-test.epfl.ch/index.fr.html"
+zwgetopt="-p"
 
 rm -Rf poubelle
 mkdir poubelle
@@ -18,49 +18,13 @@ function finish {
 }
 trap finish INT
 
-
 for i in `seq 1 $znbtests`;
 do
         echo -ne "le chargement de la page à pris "
-	time wget -p https://www-test.epfl.ch/index.fr.html --no-check-certificate -o /dev/null
+	TIMEFORMAT="%3R" ; time wget $zwgetopt $zurl --no-check-certificate -o /dev/null
         sleep $zspeed
 done
 trap - INT
 #watch -n 1 "ps ax |grep wget |wc"
 exit
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Teste si les arguments sont bien passés
-if (( $# < 1 ))
-then
-	echo -e "\nusage: ./bench1.sh nombre_de_tests_par_seconde\n"
-	exit
-fi
-
-zspeed=$1
-zsleep=`jq -n 1/$zspeed`
-
-cd poubelle
-
-for i in `seq 1 $zspeed`;
-do
-        echo $i
-	wget -p https://www-test.epfl.ch/index.fr.html --no-check-certificate -o /dev/null -b
-#	wget https://www-test.epfl.ch/index.fr.html --no-check-certificate -o /dev/null -b
-	sleep $zsleep
-done
-
 
