cf2aedd379ba88d2a5daca033b1bec2af5fd726a
Christian Zufferey <christian@zufferey.com>
2017-11-10 10:36:21 +0100

    wip

--- xmonitor.sh
+++ xmonitor.sh
@@ -22,45 +22,11 @@ trap finish INT
 for i in `seq 1 $znbtests`;
 do
         echo -ne "le chargement de la page à pris "
-	time wget -p https://www-test.epfl.ch/index.fr.html --no-check-certificate -o /dev/null
+	time wget -p -k https://www-test.epfl.ch/index.fr.html --no-check-certificate -o /dev/null
         sleep $zspeed
 done
 trap - INT
 #watch -n 1 "ps ax |grep wget |wc"
 exit
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Teste si les arguments sont bien passés
-if (( $# < 1 ))
-then
-	echo -e "\nusage: ./bench1.sh nombre_de_tests_par_seconde\n"
-	exit
-fi
-
-zspeed=$1
-zsleep=`jq -n 1/$zspeed`
-
-cd poubelle
-
-for i in `seq 1 $zspeed`;
-do
-        echo $i
-	wget -p https://www-test.epfl.ch/index.fr.html --no-check-certificate -o /dev/null -b
-#	wget https://www-test.epfl.ch/index.fr.html --no-check-certificate -o /dev/null -b
-	sleep $zsleep
-done
 
 
