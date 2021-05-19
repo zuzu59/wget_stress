f0c13b4c01b2f47ae6e9f6b53082b36f341691a0
Christian Zufferey <christian@zufferey.com>
2021-05-19 11:45:57 +0200

    Recommencé mes stress avec le site https://dev-rdp.epfl.ch pour voir s'il tient la route sur k8s ?

--- start.sh
+++ start.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 #petit script à lancer pour que cela tourne !
-#zf171127.1456
+#zf171127.1456, zf210519.1143
 
 sudo pkill -9 wget
 ./zramdisk.sh
 sleep 2
 
-zurl="https://test-www-backend.epfl.ch/index.fr.html"
+zurl="https://dev-rdp.epfl.ch"
+#zurl="https://test-www-backend.epfl.ch/index.fr.html"
 #zurl="https://test-www-proxy.epfl.ch/index.fr.html"
 #zurl="https://www.epfl.ch/index.fr.html"
-zspeed=70
-znbtests=20
+
+# zspeed=70
+# znbtests=20
+zspeed=5
+znbtests=40
 
 echo -e "./zbench.sh "$zurl $zspeed $znbtests
 ./zbench.sh $zurl $zspeed $znbtests
